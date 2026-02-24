d9380e54dc8ee72234a70f535b9dc883cc7c3d26
ketsara <ketsara.piy@ku.th>
2026-02-24 14:41:20 +0700

    [Added]:   เพิ่ม Test Api Step ค้นหาสินค้า 'Bicycle' และตรวจสอบข้อมูลชื่อ และราคาสินค้า tests/skc_shopping_mall_success.spec.ts

--- e2e-api/tests/skc_shopping_mall_success.spec.ts
+++ e2e-api/tests/skc_shopping_mall_success.spec.ts
@@ -18,6 +18,27 @@ test("ค้นหาและซื้อสินค้า เลือกว�
     test.info().attach("[POST] /login response", {
         body: JSON.stringify(await responseLogin.json()),
     });
+
+    accessToken = (await responseLogin.json()).access_token;
+  });
+
+  await test.step("ค้นหาสินค้า 'Bicycle' และตรวจสอบข้อมูลสินค้า", async () => {
+    const responseSearchProduct = await request.get(
+      "http://139.59.225.96/api/v1/product?q=Bicycle&offset=0&limit=20", {
+        headers :{
+          "Authorization": "Bearer " + accessToken,
+        }
+      }
+    );
+
+    expect(responseSearchProduct.ok()).toBeTruthy(); //200
+    expect((await responseSearchProduct.json()).products[0].product_name).toBe(
+      "Balance Training Bicycle",
+    );
+    expect(
+      (await responseSearchProduct.json()).products[0].product_price_thb,
+      "Verify product price should be equal to 4,314.60"
+    ).toBe(4314.60);
   });
   
 
