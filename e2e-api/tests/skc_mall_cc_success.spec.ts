95a8c735bad77a7ffaa403ec75d8f981a1233069
ketsara <ketsara.piy@ku.th>
2026-02-24 14:22:52 +0700

    [Added]: เพิ่ม Test Api Step ดำเนินการเข้าสู่ระบบ ด้วย username และ password ใน tests/skc_mall_cc_success.spec.ts เพื่อดำเนินการเข้าสู่ระบบ

--- e2e-api/tests/skc_mall_cc_success.spec.ts
+++ e2e-api/tests/skc_mall_cc_success.spec.ts
@@ -1,2 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+test("ค้นหาและซื้อสินค้า เลือกวิธีจัดส่งเป็น Thai Post และชำระเงินด้วยบัตรเครดิต Visa สำเร็จ", async({ request, }) => {
+  let accessToken;
+  
+  await test.step("ดำเนินการเข้าสู่ระบบ ด้วย username และ password", async() => {
+    const responseLogin = await request.post(
+      "http://139.59.225.96/api/v1/login", {
+      data: {
+        "username": "user_10",
+        "password": "P@ssw0rd",
+      },
+      }
+    );
+    expect(responseLogin.ok()).toBeTruthy(); //200
+    expect((await responseLogin.json()).access_token).toBeTruthy();
+
+    test.info().attach("[POST] /login response", {
+        body: JSON.stringify(await responseLogin.json()),
+    });
+  });
+  
+
+ 
+  
+});
